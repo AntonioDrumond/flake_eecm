1f69257707580a5b979a57b15f3aeb4a2db0f934
Antonio Drumond <adcdstom@gmail.com>
2025-04-22 14:56:25 -0300

    arduino script

--- install/post_install.sh
+++ install/post_install.sh
@@ -2,3 +2,7 @@ sudo cp ../flake.nix /etc/nixos/
 sudo cp ../configuration.nix /etc/nixos/
 sudo nixos-rebuild switch --flake /etc/nixos#nixos
 cp ../dots/.* ~
+
+cp ../scripts/* /home/seemg/Desktop/
+chmod +x /home/seemg/Desktop/*.sh
+
